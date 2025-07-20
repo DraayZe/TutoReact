bf7bdb9a15a32995af22ad88a75525ccc74ca7ec
DraayZe <lenny.fernet7@gmail.com>
2025-07-20 14:01:18 +0200

    hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const titre = "Hello, world"
 const showTitle = false
 const todos = [
@@ -14,13 +16,36 @@ function App() {
     <ul>
       {todos.map(todo => (<li key={todo}>{todo}</li>))}
     </ul>
+    <Compteur />
   </>
+
+
 }
 
 function Title({...props}) {
 
   return  <h1 style={{color:"red", backgroundColor: "blue "}} {...props}>{titre}</h1>
 
+}
+
+function Compteur() {
+
+    const [person, setPerson] = useState({
+          firstName : "John",
+          lastName : "Doe",
+          age: 30
+    }
+    )
+
+
+    const increment = () => {
+      setPerson({...person, age: person.age + 1})
+  }
+
+  return <>
+  <p>Le monsieur s'appelle {person.firstName} et son âge {person.age}</p>
+    <button onClick={increment}>Incrémenter son âge de 1 année</button>
+  </>
 }
 
 export default App
